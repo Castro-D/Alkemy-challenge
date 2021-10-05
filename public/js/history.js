ec9aa0a7b5a9ecd2603f7829381e6cb9a0f3f992
Castro-D <castro.dh91@gmail.com>
2021-10-05 15:42:01 -0300

    add delete and edit button and modal for editin registry

--- public/js/history.js
+++ public/js/history.js
@@ -8,15 +8,46 @@ function getAll() {
 async function createRows(fetchCallback) {
   const data = await fetchCallback();
   data.forEach((operation) => {
-    const template = `
-  <tr>
-    <td>${operation.id}</td>
-    <td>${operation.concept}</td>
-    <td>${operation.amount}</td>
-    <td>${operation.date}</td>
-    <td>${operation.type}</td>
-  </tr>`;
-    $tbody.innerHTML += template;
+    const row = document.createElement('tr');
+    Object.values(operation).forEach((value) => {
+      const cell = document.createElement('td');
+      const textNode = document.createTextNode(value);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+    });
+    const buttons = document.createElement('td');
+    const editLink = document.createElement('a');
+    const deleteButton = document.createElement('a');
+
+    editLink.addEventListener('click', () => {
+      const $modal = document.querySelector('.modal');
+      const $concept = document.querySelector('#concept');
+      const $id = document.querySelector('#id');
+      const $amount = document.querySelector('#amount');
+      const $date = document.querySelector('#date');
+      const $type = document.querySelector('#type');
+
+      $modal.classList.add('is-active');
+      $id.value = operation.id;
+      $concept.value = operation.concept;
+      $amount.value = operation.amount;
+      $date.value = operation.date;
+      $type.value = operation.type;
+    });
+
+    deleteButton.addEventListener('click', () => {
+      fetch(`/api/operations/delete/${operation.id}`)
+        .then(() => {
+          alert(`removed operation with id ${operation.id}`);
+          window.location.reload();
+        });
+    });
+    editLink.innerHTML = 'Edit';
+    deleteButton.innerHTML = 'Delete';
+
+    buttons.append(editLink, deleteButton);
+    row.appendChild(buttons);
+    $tbody.appendChild(row);
   });
 }
 
