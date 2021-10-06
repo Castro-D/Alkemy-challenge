9c868e784f0fd9e58dfa23388630d847adfbf3c2
Castro-D <castro.dh91@gmail.com>
2021-10-06 18:09:22 -0300

    add init module function

--- src/module/operation/module.js
+++ src/module/operation/module.js
@@ -2,7 +2,20 @@ const OperationController = require('./controller/operationController');
 const OperationService = require('./service/operationService');
 const OperationRepository = require('./repository/sqlite/operationRepo');
 
+/**
+ * @param {import('express').Application} app
+ * @param {import('rsdi').IDIContainer} container
+ */
+function init(app, container) {
+  /**
+ * @type {OperationController} controller
+ */
+  const controller = container.get('OperationController');
+  controller.configureRoutes(app);
+}
+
 module.exports = {
+  init,
   OperationController,
   OperationRepository,
   OperationService,
