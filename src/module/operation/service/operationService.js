e670d4b525cbe581ee0682b879b04aeb97e31614
Castro-D <castro.dh91@gmail.com>
2021-10-06 15:42:56 -0300

    add operation service module

---
+++ src/module/operation/service/operationService.js
@@ -0,0 +1,28 @@
+/**
+ * @typedef {import('../repository/abstractOperationRepo')} AbstractOperationRepository
+ */
+
+module.exports = class OperationService {
+  /**
+   * @param {AbstractOperationRepository} operationRepository
+   */
+  constructor(operationRepository) {
+    this.operationRepository = operationRepository;
+  }
+
+  async save(operation) {
+    return this.operationRepository.save(operation);
+  }
+
+  async dbRemove(operation) {
+    return this.operationRepository.dbRemove(operation);
+  }
+
+  async getRow(id) {
+    return this.operationRepository.getRow(id);
+  }
+
+  async getOperations() {
+    return this.operationRepository.getOperations();
+  }
+};
